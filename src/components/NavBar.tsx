13aedc8e680e11c373096748bee7a8c6c3f1b87a
Jens Ashley <38320014+JensAshley@users.noreply.github.com>
2025-04-01 16:25:23 -0400

    updae

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-column w-100">
-            <Nav.Link href="/home" style={{ color: "white" }}>
+            <Nav.Link href="#/home" style={{ color: "white" }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="40"
@@ -37,7 +37,7 @@ const NavBar = () => {
                 <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
               </svg>
             </Nav.Link>
-            <Nav.Link href="/dashboard" style={{ color: "white" }}>
+            <Nav.Link href="#/dashboard" style={{ color: "white" }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="40"
@@ -53,7 +53,7 @@ const NavBar = () => {
                 />
               </svg>
             </Nav.Link>
-            <Nav.Link href="/newpatient" style={{ color: "white" }}>
+            <Nav.Link href="#/newpatient" style={{ color: "white" }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="40"
@@ -69,7 +69,7 @@ const NavBar = () => {
                 />
               </svg>
             </Nav.Link>
-            <Nav.Link href="/account" style={{ color: "white" }}>
+            <Nav.Link href="#/account" style={{ color: "white" }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="40"
